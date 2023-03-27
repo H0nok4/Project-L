33f1f692018cb759dd0089ce9b2bcd3f912bacfc
chenzhenwei <ie2_0o26osxs1@dingtalk.com>
2023-03-27 13:27:21 +0800

    assets:基础相关

--- Project-L/Assets/Scripts/Framework/GameManager.cs
+++ Project-L/Assets/Scripts/Framework/GameManager.cs
@@ -4,8 +4,21 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    private bool IsAwake;
     private void Awake()
     {
         KeyBinding.Init();
+        UnityEditor.Compilation.CompilationPipeline.compilationStarted += CompilationStarted;
+        IsAwake = true;
+    }
+
+    private void OnDestroy() {
+        UnityEditor.Compilation.CompilationPipeline.compilationStarted -= CompilationStarted;
+        IsAwake = false;
+    }
+
+    private void CompilationStarted(object obj) {
+        UnityEditor.EditorApplication.Beep();
+        UnityEditor.EditorApplication.isPlaying = false;
     }
 }
